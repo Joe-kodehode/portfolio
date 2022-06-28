9d46bbe130c78b09563ea3ab43dd28e4968414ec
Joe Glendenning <joe@joe.joe>
2022-06-28 14:26:09 +0200

    portfolio updates

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+const styledHome = styled.main`
+  background: red;
+  width: 100vh;
+  height: 100vh;
+`;
+
+export default function Home() {
+  <styledHome>homepage</styledHome>;
+}
